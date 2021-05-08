29cd9bd70ef9f2a22dbca49eb7c8b032d0a69436
Osomhe1 <osomhe.aleogho@gmail.com>
2021-05-08 23:05:03 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+
+var stylesheet = "https://app.testdome.com/content/source/stylesheets/embed.css", link = document.createElement("link");
+     link.href = stylesheet, link.type = "text/css", link.rel = "stylesheet", link.media = "screen,print", document.getElementsByTagName("head")[0].appendChild(link);
+    
+
+
